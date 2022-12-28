6dd306e9edfbed5316a9a39d9b55670bd9dff65c
warfa@LAPTOP-BGKII3AT <warfacesdr@mail.ru>
2022-12-28 17:49:45 +0800

    Задача на поиск индекса числа в массиве чисел

--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
-﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
-//Например:
-//78 -> 8
-//12-> 2
-//85 -> 8
-int number1 = new Random().Next(10, 100);
+﻿int[] array = {1,2,3,4,5,6,7,8,99};
 
-Console.WriteLine(number1);
+int n = array.Length;
+int find = 6;
+int index = 0;
 
- if(number1 / 10 > number1 % 10) Console.WriteLine(number1/10);
- else Console.WriteLine(number1 % 10);
+ while (index < n)
+ {
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        }
+    index++;
+
+ }
+
+
 
