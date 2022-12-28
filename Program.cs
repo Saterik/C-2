55ac47c2f9f427c52033d53f60c8b267eba25174
warfa@LAPTOP-BGKII3AT <warfacesdr@mail.ru>
2022-12-28 18:06:19 +0800

    Задача на заполнение массива n числами

--- Program.cs
+++ Program.cs
@@ -1,18 +1,31 @@
-﻿int[] array = {1,2,3,4,5,6,7,8,99};
-
-int n = array.Length;
-int find = 6;
-int index = 0;
-
- while (index < n)
- {
-    if(array[index] == find)
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length) 
     {
-        Console.WriteLine(index);
-        }
-    index++;
-
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count =col.Length;
+    int position = 0;
+ while (position < count)
+ {
+Console.WriteLine(col[position]);
+position++;
  }
+}
+
+
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+
 
 
 
